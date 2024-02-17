8045e1f7c363a3e969b37a7be21d6453f38c1455
raycasimiro <54532910+raycasimiro@users.noreply.github.com>
2024-02-17 16:17:43 +0800

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  mode: "development",
+  output: { clean: true },
+  devServer: {
+    static: "./dist",
+    open: true,
+  },
+};
